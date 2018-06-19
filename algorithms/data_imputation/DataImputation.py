1699a71cafa540625c90ce3959042c1d18036078
Alessandro <ale-remo@hotmail.com>
2018-06-19 11:52:31 -0300

    Criação da classe para imputação de dados

---
+++ algorithms/data_imputation/DataImputation.py
@@ -0,0 +1,42 @@
+import numpy as np
+import pandas as pd
+from fancyimpute import KNN
+
+
+class MeanImputation:
+    description = 'Mean_Imputation'
+
+    @staticmethod
+    def impute_data(data):
+        df = pd.DataFrame(data)
+
+        df = df.fillna(np.nanmean(data))
+
+        return df[0].values.tolist()
+
+
+class KNNImputation:
+    description = 'KNN_Imputation'
+
+    @staticmethod
+    def impute_data(data, original_data):
+        matrix = [data]
+
+        for i in range(0, 250):
+            matrix.append(original_data)
+
+        full_data = KNN().complete(matrix)
+
+        print(full_data)
+        return full_data
+
+
+        # class MICEImputation:
+        #     description = 'MICE_Imputation'
+        #
+        #     @staticmethod
+        #     def impute_data(data):
+        #         full_data = IterativeSVD(gradual_rank_increase=False).complete(np.matrix([data]))
+        #
+        #         print(full_data)
+        #         return full_data
